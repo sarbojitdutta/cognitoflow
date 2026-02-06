570c199736d37b6e78e373710e93dcd69a7167bd
sarbojitdutta <sarbojitd48@gmail.com>
2026-02-07 00:18:56 +0530

    fixed and updated

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ import protect from './middlewares/auth.middleware.js'
 import webhookRoutes from './routes/webhook.route.js'
 import dashboardRoutes from './routes/dashboard.route.js'
 import userRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 connectDB()
 
@@ -22,6 +23,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/code', aiRoutes)
