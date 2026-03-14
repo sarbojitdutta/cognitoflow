116d3f2974513c6220e5eeba493db2a895579618
sarbojitdutta <sarbojitd48@gmail.com>
2026-03-14 18:22:41 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import cookieParser from 'cookie-parser'
 connectDB()
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://cognitoflow.vercel.app/"],
     credentials:true,
     methods:["GET","POST","PUT","DELETE"],
     allowedHeaders:["Content-Type","Authorization"]
